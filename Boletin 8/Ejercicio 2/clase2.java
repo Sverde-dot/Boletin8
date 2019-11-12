d642a2900ce38dfe920e66f8b5f14ad3e7be5f5d
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2019-11-12 23:28:42 +0100

    Update clase2.java

--- Boletin 8/Ejercicio 2/clase2.java
+++ Boletin 8/Ejercicio 2/clase2.java
@@ -1,77 +1,45 @@
 package boletin8.pkg2;
-
 import java.util.Scanner;
 public class clase2 {
-    Scanner sc=new Scanner(System.in);
-    private int prod;
-    private int pre=30;
-    private int des;
-    private float descuento = 0;
-    public clase2(){
-    }
+    
+private float prod;
+private int ven;
+private float des;
+private float total;
 
-   public clase2(int prod,int pre){
-        this.prod=prod;
-        this.pre=pre;
+public void pedirDatos(){
+Scanner sc = new Scanner(System.in);
+    System.out.println("Introduce las ventas:");
+    numeroVentas=sc.nextInt();
+    System.out.println("El articulo vale:");
+    prod=sc.nextInt();
+}
+public void comp(){
+    
+if (ven<100){
+des=0;
+total=prod*ven;
+}
+else if (ven>=100 && ven<200)
+   if (prod>4000){
+       total=prod*ven*0.95f;  
+       des=(prod*ven)-total;
+   }
+   else {
+   total=prod*ven*0.98f;
+   des=(prod*ven)-total;
+   }
+else  
+    if (prezoUnitario>4000){
+    total=prod*ven*0.90f;
+    des=(prod*ven)-total;
+    }
+    else {
+    total=prod*ven*0.92f;
+    des=(prod*ven)-total;    
     }    
-    public void setprod(){
-        this.prod=prod;
-    }
-    public int getprod(){
-        return prod; 
-    }
-    public void setPre(int pre) {
-        this.pre = pre;
-    }
-    public int getPre() {
-        return pre;
-    }
-    
-    
+    System.out.println("El precio del producto es de: "+prod+"\nLas ventas fueron: "+ven+"\nEl descuento es de "+des+" euros +"\nEl total a pagar es "+total+" euros");           
 
-    public void Paso(int prod){
-        System.out.println("Inserte un valor numérico entero.");
-        float respuesta = sc.nextFloat();
-        //des=(int) (respuesta-respuesta * descuento);
-        if (prod<100){
-            prod=(int) (respuesta*pre);
-
-        System.out.println("No habra descuento");
-        }
-        
-        
-        if (prod>=100&&prod<200){
-            prod=(int) (respuesta*pre);
-            /*if(pre>4000){
-                descuento = 0.05f;
-
-                System.out.println("El descuento fue de  " + des + ".");
-            }
-            else{
-                descuento = 0.02f;
-
-                System.out.println("El descuento fue de  " + des + ".");
-            }*/
-        }
-        
-        
-        if(prod>=200){
-            prod=(int) (respuesta*pre);
-                /*if(pre>4000){
-                    descuento = 0.10f;
-
-                System.out.println("El descuento fue de  " + des + ".");
-            }
-                else{
-                    descuento = 0.08f;
-
-                System.out.println("El descuento fue de  " + des + ".");
-            }*/
-        }
-        System.out.println("La cantidad insertada fue " + respuesta + ".");
-        System.out.println("El precio final es " + prod);
-
-        
-    }
+}
         
 }

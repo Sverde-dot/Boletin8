5ed66b2c474c55aa902adbddb71b2ba79f53d13b
Sverde-dot <55793525+Sverde-dot@users.noreply.github.com>
2019-11-12 23:30:26 +0100

    Update Boletin82.java

--- Boletin 8/Ejercicio 2/Boletin82.java
+++ Boletin 8/Ejercicio 2/Boletin82.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package boletin8.pkg2;
 
 /**
@@ -11,12 +7,10 @@ package boletin8.pkg2;
  */
 public class Boletin82 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        clase2 obx = new clase2();
-        obx.Paso(0);  
+        clase2 obx= new clase2();
+        obx.pedirDatos();
+        obx.comp();
     }
     
 }
